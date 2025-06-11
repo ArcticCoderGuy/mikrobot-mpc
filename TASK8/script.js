9e1018845a2fd026c83a4eeb22a5a810c6296ea6
Your Name <you@example.com>
2025-06-11 15:08:50 +0300

    Testailua

--- TASK8/script.js
+++ TASK8/script.js
@@ -1,4 +1,24 @@
 function changeImage() {
-  const selected = document.getElementById("colorSelect").value;
-  document.getElementById("roseImage").src = `img/${selected}.jpg`;
+  const color = document.getElementById("colorSelect").value;
+  const image = document.getElementById("roseImage");
+  image.src = `img/${color}.jpg`;
 }
+
+const image = document.getElementById("roseImage");
+const list = document.getElementById("notificationList");
+
+image.addEventListener("mouseover", () => {
+  image.classList.add("enlarged");
+});
+
+image.addEventListener("mouseout", () => {
+  image.classList.remove("enlarged");
+  list.innerHTML = "";
+});
+
+image.addEventListener("click", () => {
+  const color = document.getElementById("colorSelect").value;
+  const li = document.createElement("li");
+  li.textContent = `You clicked the ${color} rose`;
+  list.appendChild(li);
+});
